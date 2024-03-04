bf8d26dbbee951558b4f8e1b65f771fc5d075107
lils-h-lils <lilymaih12@gmail.com>
2024-03-04 19:01:03 +0000

    Random number generator

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -11,7 +11,10 @@ namespace CMP1903_A1_2324
         //Property to hold the current die value
         public int FaceValue { get; private set; }
 
-       
+        //Random number generator 
+        private Random random;
+
+
         /*
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
