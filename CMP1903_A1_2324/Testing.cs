ae52aa1765814dd752b66a6982981ce5db54bafe
lils-h-lils <lilymaih12@gmail.com>
2024-03-04 19:39:15 +0000

    the next method tests the Game object

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -23,5 +23,14 @@ namespace CMP1903_A1_2324
             Debug.Assert(rollValue >= 1 && rollValue <= 6, "Die roll should e betweeen 1 and 6.");
             Console.WriteLine("Die roll test passed.");
         }
+
+        //Method to test the Game class
+        private static void TestGame()
+        {
+            Game game = new Game();
+            int total = game.RollDice();
+            Debug.Assert(total >= 3 && total <= 18, "Sum of dice rolls should be between 3 and 18");
+            Console.WriteLine("Game test passed.");
+        }
     }
 }
