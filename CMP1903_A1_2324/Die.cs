292740f013a1c0130f280fcb21e5b0217bd7fb19
lils-h-lils <lilymaih12@gmail.com>
2024-03-05 20:36:54 +0000

    Created XML documenting comments in the 'Die' class

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -5,23 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
-{
+{   /// <summary>
+    /// Means a die with a face value that can be rolled
+    /// </summary>
     internal class Die
     {
-        //Property to hold the current die value
+        /// <summary>
+        /// Recieves the current face value of the die
+        /// </summary>
         public int FaceValue { get; private set; }
 
         //Random number generator 
         private Random random;
 
-        //Constructor 
+        ///<summary>
+        /// Initalises a new instance of 'Die' class
+        /// </summary> 
         public Die()
         {
             random = new Random();
             Roll(); //Roll the die when it's created 
         }
 
-        //Method to roll the die 
+        ///<summary>
+        /// Rolls the die and sets its face value to a random number between 1 and 6 (inclusive).
+        /// </summary> 
+        /// <returns>The face value of the die after rolling</returns>
         public int Roll()
         {
             //Generate a random number between 1 and 6 (inclusive)
